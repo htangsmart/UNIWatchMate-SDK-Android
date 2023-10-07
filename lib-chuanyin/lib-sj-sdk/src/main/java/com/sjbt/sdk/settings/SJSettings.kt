3a9042a35f083b56276b4ba3ea3e4526b8c497e7
wangrui <Ray>
2023-10-07 11:21:59 +0800

    增加睡眠设置实现

--- lib-chuanyin/lib-sj-sdk/src/main/java/com/sjbt/sdk/settings/SJSettings.kt
+++ lib-chuanyin/lib-sj-sdk/src/main/java/com/sjbt/sdk/settings/SJSettings.kt
@@ -17,6 +17,5 @@ class SJSettings(sjUniWatch: SJUniWatch) : AbWmSettings() {
     override val settingAppView: AbWmSetting<WmAppView> = SettingAppView(sjUniWatch)
     override val settingDrinkWater: AbWmSetting<WmSedentaryReminder> = SettingSedentaryReminder()
     override val settingHeartRate: AbWmSetting<WmHeartRateAlerts> = SettingHeartRateAlerts()
-    override val settingSleepSettings: AbWmSetting<WmSleepSettings>
-        get() = TODO("Not yet implemented")
+    override val settingSleepSettings: AbWmSetting<WmSleepSettings> = SettingSleepSet(sjUniWatch)
 }
