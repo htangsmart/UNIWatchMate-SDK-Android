986497cce10cf86b4ee7e0879c14600f0b5c87c0
祁亚超 <qiyachao@shenjugroup.com>
2023-10-08 19:02:52 +0800

    fixbug

--- app-Sample/src/main/java/com/sjbt/sdk/sample/utils/PermissionHelper.kt
+++ app-Sample/src/main/java/com/sjbt/sdk/sample/utils/PermissionHelper.kt
@@ -67,6 +67,8 @@ object PermissionHelper {
             //>=31
             else -> {
                 arrayListOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.BLUETOOTH_SCAN,
                     Manifest.permission.BLUETOOTH_CONNECT
                 )
